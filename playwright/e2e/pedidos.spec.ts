ec4045b043f01feece15694fa556168b6ed5db74
Jessica Machado <jeh-machado@outlook.com>
2026-04-21 17:13:41 -0300

    :test_tube  Primeiro teste de cosulta de pedido aprovado

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+
+test('deve consultar um pedido aprovado', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  
+  // Checkpoint 1: Verificar se a página de landing está carregada
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+  
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+
+  // Checkpoint 2: Verificar se a página de consulta de pedidos está carregada  
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+
+
+  await page.getByTestId('search-order-id').fill('VLO-G9MK3C');
+
+  await page.getByTestId('search-order-button').click();
+  await expect(page.getByTestId('order-result-id')).toBeVisible();
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-G9MK3C');
+  await expect(page.getByTestId('order-result-status')).toBeVisible();
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+  
+});
