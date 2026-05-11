97792a3a2060366a79541a8d03e619c215bcb0b2
Jessica Machado <jeh-machado@outlook.com>
2026-05-11 09:34:56 -0300

    📑Adicionando resolução do Papito no desafio proposto anteriormente

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -14,11 +14,29 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await page.getByRole('button', { name: 'Buscar Pedido' }).click();
 
 
-   // Assert (Verificar)
+  // Assert (Verificar)
   const infoPedido = page.locator('//div[@data-testid="order-result-VLO-G9MK3C"]//p[text()="Pedido"]/following-sibling::p[1]');
   await expect(infoPedido).toBeVisible({timeout: 10_000});
   await expect(infoPedido).toContainText('VLO-G9MK3C');
 
-  await expect(page.getByText('APROVADO')).toBeVisible();
   
+  /****************************/
+  /* IMPLEMENTAÇÕES DO PAPITO */
+  /****************************/
+  //1) Xpath puro na resolução
+  /*
+  const orderCode = page.locator('//p[text()="Pedido"]/..//p[text()="VLO-G9MK3C"]')
+  await expect(orderCode).toBeVisible({timeout: 10_000});
+  */
+
+   
+  //2) Utilizar getByRole para utilizar o parágrafo e inserir filtro pela subfunção 'filter"
+  /*
+  const containerPedido = page.getByRole('paragraph')
+  .filter({hasText: 'Pedido'})
+  .locator('..') // Sobe para o elemento pai (a div que agrupa ambos)
+  await expect(containerPedido).toBeVisible({timeout: 10_000});
+  */
+
+  await expect(page.getByText('APROVADO')).toBeVisible();  
 });
