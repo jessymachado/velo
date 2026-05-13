c573e1b56cfaf8d3b6e46f9316c5532d17410697
Jessica Machado <jeh-machado@outlook.com>
2026-05-13 17:04:47 -0300

    :art: Conclusão aplicação do padrão page objects

--- playwright/support/pages/OrderLockupPage.ts
+++ playwright/support/pages/OrderLockupPage.ts
@@ -86,4 +86,8 @@ export class OrderLockupPage {
             - paragraph: Verifique o número do pedido e tente novamente
           `);
     }
+
+    async validatePageLoaded(){
+        await expect(this.page.getByRole('heading')).toContainText('Consultar Pedido');
+    }
 }
